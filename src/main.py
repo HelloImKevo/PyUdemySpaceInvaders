2f1a911e47a36406bee80f21996b866ada1ade68
Kevin Schanz <kevinschanz@live.com>
2021-04-22 18:59:38 -0400

    Set PyGame window title and add UFO icon

--- src/main.py
+++ src/main.py
@@ -24,9 +24,18 @@ pygame.init()
 # Create the Screen
 screen = pygame.display.set_mode(size=(800, 600))
 
+# Title and Icon
+pygame.display.set_caption("Space Invaders")
+icon = pygame.image.load("ufo.png")
+pygame.display.set_icon(icon)
+
 # Game Loop
 running = True
 while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
+
+    # RGB - Red, Green, Blue
+    screen.fill(color=(0, 0, 0))
+    pygame.display.update()
